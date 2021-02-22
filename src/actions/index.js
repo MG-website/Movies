d9e3e5cb972d414e3820f54096451d1e143e4ec5
MG <marcosg1996@hotmail.com.ar>
2021-02-22 10:41:01 -0300

    variable de entorno

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+const API_URL = process.env.REACT_APP_API_URL
 
 export function addMovieFavorite(payload) {
     return { type: "ADD_MOVIE_FAVORITE", payload };
@@ -7,7 +7,8 @@ export function addMovieFavorite(payload) {
   
   export function getMovies(titulo) {
     return function(dispatch) {
-      return axios.get("http://www.omdbapi.com/?apikey=20dac387&s=" + titulo)
+      console.log(API_URL)
+      return axios.get(API_URL + " &s=" + titulo)
     
         .then(json => {
           dispatch({ type: "GET_MOVIES", payload: json.data });
@@ -19,7 +20,7 @@ export function addMovieFavorite(payload) {
 /*     return{ type: 'GET_MOVIES_DETAIL', payload}
  */  
 return function(dispatch) {
-  return fetch("http://www.omdbapi.com/?apikey=20dac387&i=" + id)
+  return fetch(API_URL + " &i=" + id)
     .then(response => response.json())
     .then(json => {
       dispatch({ type: "GET_MOVIES_DETAIL", payload: json });
